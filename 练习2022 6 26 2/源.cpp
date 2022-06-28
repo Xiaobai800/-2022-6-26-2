e5cd8df36e5aa5ebf0b65d3b2fda5c08366b0106
Xiaobai800 <2533760032@qq.com>
2022-06-28 11:42:12 +0800

    添加项目文件。

---
+++ 练习2022 6 26 2/源.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+struct Book
+{
+	char name[20];//C语言程序设计
+	short price;
+};
+
+int main()
+{
+	struct Book b1 = { "C语言程序设计"，55};
+	printf("书名:%s\n", b1.name);
+	printf("价格:%d\n", b1.price);
+
+
+
+	return 0;
+}
